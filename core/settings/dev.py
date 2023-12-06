f03c610e1fa899bc16eb44c05cef49e3748ae41d
Sabbir <sabbirhasan2999@gmail.com>
2023-12-07 01:17:05 +0600

    .

--- core/settings/dev.py
+++ core/settings/dev.py
@@ -16,11 +16,7 @@ ALLOWED_HOSTS = []
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-]
-
-CSRF_TRUSTED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
+    "http://192.168.1.8:3000",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
