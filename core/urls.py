afb82e3a66b5e1eebebd4704d434ec30afbee53f
Sabbir <sabbirhasan2999@gmail.com>
2023-12-31 15:33:48 +0600

    nonetype staff handled

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,13 @@ from django.contrib import admin
 from django.db import router
 from django.urls import path, include
 
+
+# Admin Site Config
+admin.site.site_header = "Zenith System"
+admin.site.site_title = "Zenith System"
+admin.site.index_title = "Welcome to Zenith System Administrations"
+
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
--- management/signals.py
+++ management/signals.py
@@ -1,18 +1,27 @@
-import logging
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.db import models
-from management.models import Task, Staff
+from management.models import Staff, Task
 from main.models import Reservation
 
-# Get an instance of a logger
+import logging
+
 logger = logging.getLogger(__name__)
 
 
 @receiver(post_save, sender=Reservation)
 def create_task_on_checked_out(sender, instance, **kwargs):
+    """
+    Signal handler to create a task when a reservation is checked out.
+
+    Args:
+        sender: The sender of the signal (Reservation model).
+        instance: The instance of the Reservation model being saved.
+        kwargs: Additional keyword arguments.
+
+    Returns:
+        None
+    """
     if instance.reservation_status == Reservation.ReservationStatusChoices.CHECKED_OUT:
-        # Check if there is available staff
         available_staff = Staff.objects.filter(
             staff_status=Staff.StuffStatusChoices.AVAILABLE
         ).first()
@@ -20,20 +29,60 @@ def create_task_on_checked_out(sender, instance, **kwargs):
         if available_staff:
             Task.objects.create(assigned_room=instance.room, staff=available_staff)
         else:
+            Task.objects.create(assigned_room=instance.room)
             logger.warning("No available staff found for reservation %s", instance)
 
 
 @receiver(post_save, sender=Task)
 def update_staff_on_task_status_change(sender, instance, **kwargs):
+    """
+    Signal handler to update staff status based on changes in task status.
+
+    Args:
+        sender: The sender of the signal (Task model).
+        instance: The instance of the Task model being saved.
+        kwargs: Additional keyword arguments.
+
+    Returns:
+        None
+    """
     staff = instance.staff
 
-    if instance.task_status == Task.TaskStatusChoices.PENDING:
-        staff.staff_status = Staff.StuffStatusChoices.ASSIGNED
-    elif instance.task_status == Task.TaskStatusChoices.IN_PROGRESS:
-        staff.staff_status = Staff.StuffStatusChoices.BUSY
-    elif instance.task_status == Task.TaskStatusChoices.COMPLETED:
-        staff.staff_status = Staff.StuffStatusChoices.AVAILABLE
-    else:
-        print("Invalid task status")
+    if staff:
+        if instance.task_status == Task.TaskStatusChoices.PENDING:
+            staff.staff_status = Staff.StuffStatusChoices.ASSIGNED
+        elif instance.task_status == Task.TaskStatusChoices.IN_PROGRESS:
+            staff.staff_status = Staff.StuffStatusChoices.BUSY
+        elif instance.task_status == Task.TaskStatusChoices.COMPLETED:
+            staff.staff_status = Staff.StuffStatusChoices.AVAILABLE
+        else:
+            logger.warning("Invalid task status for task %s", instance)
+    elif staff is None:
+        logger.warning("No staff found for task %s", instance)
+        return
 
     staff.save()
+
+
+@receiver(post_save, sender=Staff)
+def assign_staff_to_pending_tasks(sender, instance, **kwargs):
+    """
+    Signal handler to assign available staff to pending tasks.
+
+    Args:
+        sender: The sender of the signal (Staff model).
+        instance: The instance of the Staff model being saved.
+        kwargs: Additional keyword arguments.
+
+    Returns:
+        None
+    """
+    if instance.staff_status == Staff.StuffStatusChoices.AVAILABLE:
+        # Find tasks with no assigned staff
+        pending_tasks = Task.objects.filter(staff=None)
+
+        if pending_tasks.exists():
+            # Assign the available staff to the oldest pending task
+            oldest_task = pending_tasks.order_by("created_at").first()
+            oldest_task.staff = instance
+            oldest_task.save()
