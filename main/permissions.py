b702d1fa5f67e5550e809a6bd65c9fc99d56f526
Sabbir <sabbirhasan2999@gmail.com>
2023-12-31 16:59:26 +0600

    .

--- main/permissions.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import permissions
-
-
-class IsGuest(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return request.user.is_authenticated and hasattr(request.user, "guest")
--- main/views.py
+++ main/views.py
@@ -1,6 +1,5 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
-from main.permissions import IsGuest
 from .models import (
     Guest,
     Floor,
