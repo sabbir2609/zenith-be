28a720b798c438f7332e4052181f71d1ccdc1688
Sabbir2609 <sabbirhasan2999@gmail.com>
2024-02-09 02:07:44 +0600

    received done

--- iot/channels/consumers.py
+++ iot/channels/consumers.py
@@ -1,28 +1,44 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
-from iot.tasks import mqtt_client_task
+from iot.tasks import mqtt_client_task, publish_to_mqtt
+
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class DeviceConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.device_id = self.scope["url_route"]["kwargs"]["device_id"]
-        self.room_group_name = f"{self.device_id}"
+        try:
+            self.device_id = self.scope["url_route"]["kwargs"]["device_id"]
+            self.room_group_name = f"{self.device_id}"
 
-        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
-        await self.accept()
+            await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+            await self.accept()
 
-        device_client_id = self.device_id
-        self.mqtt_client_task_result = mqtt_client_task.delay(device_client_id)
+            self.mqtt_client_task_result = await mqtt_client_task.delay(self.device_id)
+        except Exception as e:
+            logger.error(f"Failed to connect: {e}")
 
     async def on_message(self, event):
         message = event["message"]
         await self.send(text_data=json.dumps({"message": message}))
 
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        received_data = data["state"]
+        logger.info(f"Received data: {received_data}")
+        try:
+            self.mqtt_client_task_result = await publish_to_mqtt.delay(
+                self.device_id, received_data
+            )
+        except Exception as e:
+            logger.error(f"Failed to process received data: {e}")
+
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+        print(f"Connection closed for: {self.device_id}")
 
         if hasattr(self, "mqtt_client_task_result"):
             self.mqtt_client_task_result.revoke(terminate=True)
-
-    async def on_receive(self, text_data):
-        pass
+            logger.info("Disconnected from MQTT Broker!")
--- iot/tasks.py
+++ iot/tasks.py
@@ -1,10 +1,16 @@
 # iot/task.py
 
+import json
 from celery import shared_task
 import paho.mqtt.client as mqtt
 from asgiref import sync
 from channels.layers import get_channel_layer
 from django.conf import settings
+import paho.mqtt.publish as publish
+
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 @shared_task(bind=True)
@@ -13,17 +19,19 @@ def mqtt_client_task(self, device_client_id):
 
     device = Device.objects.get(client_id=device_client_id)
     topic = device.topic
-    print(topic)
 
     def on_connect(client, userdata, flags, rc):
         if rc == 0:
-            print("Connected to MQTT Broker!", rc)
+            logger.info("Connected to MQTT Broker! RC: %d", rc)
         else:
-            print("Failed to connect, return code %d\n", rc)
+            logger.error("Failed to connect, return code %d", rc)
         client.subscribe(f"{topic}")
 
+    def on_disconnect(client, userdata, rc):
+        logger.info("Disconnected from MQTT Broker! RC: %d", rc)
+
     def on_message(client, userdata, msg):
-        print(msg.payload.decode())
+        logger.info("Received message: %s", msg.payload.decode())
 
         channel_layer = get_channel_layer()
         sync.async_to_sync(channel_layer.group_send)(
@@ -34,7 +42,29 @@ def mqtt_client_task(self, device_client_id):
     client = mqtt.Client()
 
     client.on_connect = on_connect
+    client.on_disconnect = on_disconnect
     client.on_message = on_message
 
     client.connect(settings.MQTT_HOST, settings.MQTT_PORT, settings.MQTT_KEEPALIVE)
     client.loop_forever()
+
+
+@shared_task(bind=True)
+def publish_to_mqtt(self, device_client_id, received_data):
+    from iot.models import Device
+
+    device = Device.objects.get(client_id=device_client_id)
+    topic = device.topic
+
+    mqtt_message = json.dumps({"message": received_data})
+
+    try:
+        publish.single(
+            topic,
+            payload=mqtt_message,
+            hostname=settings.MQTT_HOST,
+            port=settings.MQTT_PORT,
+        )
+        logger.info("Published to MQTT: Topic='%s', Message='%s'", topic, mqtt_message)
+    except Exception as e:
+        logger.error(f"Error publishing to MQTT: {e}")
