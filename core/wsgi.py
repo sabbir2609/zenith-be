5a3ec77d2a8ce60e02eb5001431a31c0fc2a8973
Sabbir2609 <sabbirhasan2999@gmail.com>
2024-02-26 00:46:50 +0600

    Add vercel deployment configuration

--- core/wsgi.py
+++ core/wsgi.py
@@ -5,3 +5,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
 application = get_wsgi_application()
+
+
+# for vercel deployment
+app = application
