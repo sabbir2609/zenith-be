67db6ea6ec1d57b1873b1d0e9a85627e5d632ee1
Sabbir2609 <sabbirhasan2999@gmail.com>
2024-02-26 00:54:15 +0600

    Update DJANGO_SETTINGS_MODULE in wsgi.py

--- core/wsgi.py
+++ core/wsgi.py
@@ -2,7 +2,11 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+settings_module = (
+    "core.settings.prod" if "WEBSITE_HOSTNAME" in os.environ else "core.settings.dev"
+)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
 
 application = get_wsgi_application()
 
