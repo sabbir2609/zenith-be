6ec0ec4faba0cb813654a78d3bb5a2d1829c8f56
Sabbir2609 <sabbirhasan2999@gmail.com>
2024-02-29 10:26:09 +0600

    Update database settings in dev.py

--- core/settings/dev.py
+++ core/settings/dev.py
@@ -59,6 +59,7 @@ DATABASES = {
 # }
 
 # Production database settings: For migration and creating superuser
+
 # DATABASES = {
 #     "default": dj_database_url.parse(os.environ.get("DATABASE_URL"), conn_max_age=600),
 # }
