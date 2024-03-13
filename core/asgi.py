f245a9702345b0ac65437eb74da2c7fde746a17c
Sabbir2609 <sabbirhasan2999@gmail.com>
2024-03-13 22:29:46 +0600

    Update ASGI configuration to include AllowedHostsOriginValidator

--- core/asgi.py
+++ core/asgi.py
@@ -1,6 +1,7 @@
 import os
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+from channels.security.websocket import AllowedHostsOriginValidator
 
 settings_module = (
     "core.settings.prod" if "WEBSITE_HOSTNAME" in os.environ else "core.settings.dev"
@@ -12,5 +13,8 @@ from core.routing import websocket_urlpatterns  # noqa isort:skip
 
 
 application = ProtocolTypeRouter(
-    {"http": get_asgi_application(), "websocket": URLRouter(websocket_urlpatterns)}
+    {
+        "http": get_asgi_application(),
+        "websocket": AllowedHostsOriginValidator(URLRouter(websocket_urlpatterns)),
+    }
 )
