d63826f2d3f351cddb689d1ac76a7fa94cef5d6b
Sabbir2609 <sabbirhasan2999@gmail.com>
2024-03-16 22:39:14 +0600

    Enable pagination in REST framework settings

--- core/settings/base.py
+++ core/settings/base.py
@@ -130,8 +130,8 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_AUTHENTICATION_CLASSES": ("user.authentication.CustomJWTAuthentication",),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
-    # "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
-    # "PAGE_SIZE": 10,
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "PAGE_SIZE": 10,
 }
 
 AUTH_USER_MODEL = "user.User"
