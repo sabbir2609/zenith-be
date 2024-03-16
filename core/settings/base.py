f8f8b66affb6efc61bdf6d72bb8d768b276c537c
Sabbir2609 <sabbirhasan2999@gmail.com>
2024-03-16 23:20:54 +0600

    Update pagination class in REST_FRAMEWORK settings

--- core/settings/base.py
+++ core/settings/base.py
@@ -130,7 +130,7 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_AUTHENTICATION_CLASSES": ("user.authentication.CustomJWTAuthentication",),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
 }
 
