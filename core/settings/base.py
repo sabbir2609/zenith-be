88fb06927ae2e9948ba9aefe18667ce4bd9f6e91
Sabbir2609 <sabbirhasan2999@gmail.com>
2024-03-18 22:00:59 +0600

    Update settings and views***

--- core/settings/base.py
+++ core/settings/base.py
@@ -130,8 +130,6 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_AUTHENTICATION_CLASSES": ("user.authentication.CustomJWTAuthentication",),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 10,
 }
 
 AUTH_USER_MODEL = "user.User"
--- iot/views.py
+++ iot/views.py
@@ -2,6 +2,7 @@ from django.views.generic import TemplateView
 
 from rest_framework import viewsets
 
+from iot.pagination import DefaultPagination
 from iot.models import DeviceType, Device, RoomDevice, FacilityDevice
 from iot.serializers import (
     DeviceTypeSerializer,
@@ -19,6 +20,9 @@ class DeviceTypeViewSet(viewsets.ModelViewSet):
 
 class DeviceViewSet(viewsets.ModelViewSet):
     queryset = Device.objects.all()
+    pagination_class = DefaultPagination
+    search_fields = ["name", "client_id", "topic"]
+    ordering_fields = ["installation_date", "status", "qos", "created_at"]
 
     def get_serializer_class(self):
         if self.action == "list":
