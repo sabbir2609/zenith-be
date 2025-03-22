aef036db83d9e6ae959f4a8a7223e27e4f59fe50
Sabbir Hasan Munna <sabbirhasan2999@gmail.com>
2025-03-23 01:35:28 +0600

    feat: update API endpoints for user registration and data management, enhance authentication handling, and modify user serializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,4 +1,7 @@
-from djoser.serializers import UserCreateSerializer
+from djoser.serializers import (
+    UserCreateSerializer,
+    UserSerializer as DjoserUserSerializer,
+)
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
@@ -10,8 +13,8 @@ class UserCreateSerializer(UserCreateSerializer):
         fields = ("id", "email", "first_name", "last_name", "password")
 
 
-class UserSerializer(UserCreateSerializer):
-    class Meta(UserCreateSerializer.Meta):
+class UserSerializer(DjoserUserSerializer):
+    class Meta(DjoserUserSerializer.Meta):
         model = User
         fields = (
             "id",
